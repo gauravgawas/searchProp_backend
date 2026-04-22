ce0594444814ed9391148df9cdadf441a687921d
gauravgawas <gauravgawas9999@gmail.com>
2026-04-22 18:54:07 +0530

    changed db config for aws

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,21 @@
 import mysql from "mysql2/promise";
 
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "gG@7350563644",
+//   database: "auth",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// });
+
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "gG@7350563644",
-  database: "auth",
+  host: "database-1.c4jiuuy40jjq.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "gaurav47",
+  database: "auth", // you need to create this
   waitForConnections: true,
+  port: 3306,
   connectionLimit: 10,
 });
 
