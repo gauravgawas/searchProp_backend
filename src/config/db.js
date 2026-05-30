d110cf7a7f1f0e746fc6adea73ca5c68a7d9783a
gauravgawas <gauravgawas9999@gmail.com>
2026-05-30 12:20:47 +0530

    db diverted to locally hosted on ec2

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,25 @@
 import mysql from "mysql2/promise";
 
+const pool = mysql.createPool({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "auth",
+  waitForConnections: true,
+  connectionLimit: 10,
+  connectTimeout: 60000,
+});
+
 // const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "gG@7350563644",
-//   database: "auth",
+//   host: "database-1.c4jiuuy40jjq.us-east-1.rds.amazonaws.com",
+
+//   user: "admin",
+//   password: "gaurav_aws47",
+//   database: "auth", // you need to create this
 //   waitForConnections: true,
+//   port: 3306,
 //   connectionLimit: 10,
 // });
-
-const pool = mysql.createPool({
-  host: "database-1.c4jiuuy40jjq.us-east-1.rds.amazonaws.com",
-  user: "admin",
-  password: "gaurav47",
-  database: "auth", // you need to create this
-  waitForConnections: true,
-  port: 3306,
-  connectionLimit: 10,
-});
 
 export default pool;
